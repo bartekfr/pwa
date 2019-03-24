cff15c84fe6fff65e3076ce04c6c33cdc6765988
Bartek Fryzowicz <bartekfr@wp.pl>
2019-03-24 19:00:47 +0100

    Caches app.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,12 @@
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+  	caches.open('static').then(cache => {
+		console.log('[SW] Precaching')
+		cache.add('/src/js/app.js')
+  	})
+  )
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,7 +15,7 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
+  // console.log('[Service Worker] Fetching something ....', event);
   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
   event.respondWith(fetch(event.request));
 });
