d24fd538a80ee370343334a3153720e8e0291121
Bartek Fryzowicz <bartekfr@wp.pl>
2019-09-09 11:11:30 +0200

    Cache multiple files with addAll

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,26 @@
 
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(
-  		caches.open('static').then(cache => {
-		console.log('[SW] Precaching')
-		cache.add('/src/js/app.js')
-		cache.add('/src/css/feed.css')
-  	})
-  )
-});
+	console.log('[Service Worker] Installing Service Worker ...', event);
+	event.waitUntil(
+		caches.open('static')
+			.then(function(cache) {
+				console.log('[Service Worker] Precaching App Shell');
+				cache.addAll([
+					'/',
+					'/index.html',
+					'/src/js/app.js',
+					'/src/js/feed.js',
+					'/src/js/material.min.js',
+					'/src/css/app.css',
+					'/src/css/feed.css',
+					'/src/images/main-image.jpg',
+					'https://fonts.googleapis.com/css?family=Roboto:400,700',
+					'https://fonts.googleapis.com/icon?family=Material+Icons',
+					'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+				]);
+			})
+	)
+})
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ...', event);
@@ -19,13 +31,13 @@ self.addEventListener('fetch', function(event) {
   // console.log('[Service Worker] Fetching something ....', event);
   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
   event.respondWith(
-  	caches.match(event.request)
-  		.then(response => {
-  			if (response) {
-  				return response;
-  			} else {
-  				return fetch(event.request)
-  			}
-  		})
+	caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			} else {
+				return fetch(event.request)
+			}
+		})
   );
 });
